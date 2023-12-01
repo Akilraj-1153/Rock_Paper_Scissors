8a0695499b043402c7797d01f7ac8559dcb81545
AKILRAJ  N <122591298+Akilraj-1153@users.noreply.github.com>
2023-12-01 23:43:26 +0530

    final output

--- src/RPSGame.js
+++ src/RPSGame.js
@@ -1,19 +1,25 @@
-
 import { useState } from 'react';
 import './App.css';
 
-function RPSGame({ }) {
+function RPSGame() {
+  const [roundNo, setRoundno] = useState(0);
+  const [playerScore, setPlayerscore] = useState(0);
+  const [computerScore, setComputerscore] = useState(0);
+  const [tiescore, setTiescore] = useState(0);
+  const [comCho, setComcho] = useState("Loading..");
+  const [report, setReport] = useState("Start the Game!");
+  const [winner, setWinner] = useState("Loading...");
+  const [counter, setCounter] = useState(0);
 
- const[roundNo,setRoundno]=useState(0);
- const[playerScore,setPlayerscore]=useState(0);
- const[computerScore,setComputerscore]=useState(0);
- const[tiescore,setTiescore]=useState(0);
-
- const[comCho,setComcho]=useState("Loading..");
- const[report,setReport]=useState("Start the Game!");
-
-  function Newgame(){
-    window.location.reload(false);
+  function Newgame() {
+    setRoundno(0);
+    setPlayerscore(0);
+    setComputerscore(0);
+    setTiescore(0);
+    setComcho("Loading..");
+    setReport("Start the Game!");
+    setWinner("Loading...");
+    setCounter(0);
   }
 
   function getComputerChoice() {
@@ -25,88 +31,86 @@ function RPSGame({ }) {
 
   function playRound(PlayerChoice, computerSelection) {
     if (PlayerChoice === computerSelection) {
-        tie+=1
-        return "Tie";  
-    } 
-    else if (
-        (PlayerChoice === "Rock" && computerSelection === "Scissor") ||
-        (PlayerChoice === "Paper" && computerSelection === "Rock") ||
-        (PlayerChoice === "Scissor" && computerSelection === "Paper")
-        
+      setTiescore((prevTiescore) => prevTiescore + 1);
+      setReport(`Tie! ${PlayerChoice} and ${computerSelection} are the same`);
+    } else if (
+      (PlayerChoice === "Rock" && computerSelection === "Scissor") ||
+      (PlayerChoice === "Paper" && computerSelection === "Rock") ||
+      (PlayerChoice === "Scissor" && computerSelection === "Paper")
     ) {
-        player += 1;
-        return "You Won! " + PlayerChoice + " beats " + computerSelection;
-    } else if(
-        (PlayerChoice === "Scissor" && computerSelection === "Rock") ||
-        (PlayerChoice === "Paper" && computerSelection === "Scissor") ||
-        (PlayerChoice === "Rock" && computerSelection === "Paper")
-    ){
-        computer += 1;
-        return "You Lose! " + computerSelection + " beats " + PlayerChoice;
+      setPlayerscore((prevPlayerScore) => prevPlayerScore + 1);
+      setReport(`You Won! ${PlayerChoice} beats ${computerSelection}`);
+    } else if (
+      (PlayerChoice === "Scissor" && computerSelection === "Rock") ||
+      (PlayerChoice === "Paper" && computerSelection === "Scissor") ||
+      (PlayerChoice === "Rock" && computerSelection === "Paper")
+    ) {
+      setComputerscore((prevComputerScore) => prevComputerScore + 1);
+      setReport(`Computer Won! ${computerSelection} beats ${PlayerChoice}`);
     }
-}
+  }
 
-function game(PlayerChoice) {
-  if(roundNo<=Roundlimit){
-  const computerSelection = getComputerChoice();
-  var roundResult = playRound(PlayerChoice, computerSelection);
-  console.log(roundResult);
-  
-  if(roundNo == Roundlimit || player == 3 || computer == 3){
-      if (player >= 3 || player > computer) {
-          
-      } else if (computer >= 3 || player < computer) {
-          
-      } else {
-          
+  function game(PlayerChoice) {
+    setRoundno(roundNo + 1);
+    console.log(roundNo);
+    setCounter(counter + 1);
+
+    if (counter > 4) {
+      Newgame();
+    } else {
+      const computerSelection = getComputerChoice();
+      setComcho(computerSelection);
+      playRound(PlayerChoice, computerSelection);
+      console.log(roundNo);
+      if(roundNo === 4){
+          if(playerScore === computerScore){
+            setWinner("Match Draw...");
+          }
+          else if(playerScore>computerScore){
+            setWinner("Player");
+          }
+          else if(playerScore<computerScore){
+            setWinner("Computer");
+          }
       }
+    }
   }
-  Roundno++;
-  }
-}
+  
 
-let Roundlimit=5;
-let Roundno = 1;  
-let tie=0;
-let player = 0;
-let computer = 0;
-
-  return(
+  return (
     <div>
-        <h1 className='Title'>ROCK PAPER SCISSOR</h1>
-        <div className='playerip'>
-          <h2>CHOOSE YOUR TOOL</h2>
-          <div className='inputbutton'>
-            <button onClick={game("Rock")}>ROCK</button>
-            <button onClick={game("Paper")}>PAPER</button>
-            <button onClick={game("Scissor")}>SCISSOR</button>
-          </div>
+      <h1 className='Title'>ROCK PAPER SCISSOR</h1>
+      <div className='playerip'>
+        <h2>CHOOSE YOUR TOOL</h2>
+        <div className='inputbutton'>
+          <button onClick={() => game("Rock")}>ROCK</button>
+          <button onClick={() => game("Paper")}>PAPER</button>
+          <button onClick={() => game("Scissor")}>SCISSOR</button>
         </div>
-        <div >
-          <h2>COMPUTER CHOOSING</h2>
-          <button className='comchoose'>{comCho}</button>
+      </div>
+      <div>
+        <h2>COMPUTER CHOOSING</h2>
+        <button className='comchoose'>{comCho}</button>
+      </div>
+      <div className='Reportbox'>
+        <div className='report'>
+        <h2>Round:<span >{roundNo}</span></h2>
+          <h2><span className='reportstatus'>{report}</span></h2>
         </div>
+        <div className='scorecard'>
+          <h2 >Player <span id="playerScore">{playerScore}</span></h2>
+          <h2 >Computer  <span id="computerScore">{computerScore}</span></h2>
+          <h2 >Tie  <span id="tieScore">{tiescore}</span></h2>
+        </div>
+      </div>
 
-        <div className='Reportbox'>
-          <div className='report'>
-              <h2>Round:<span >{roundNo}</span></h2>
-              <h2><span>{report}</span></h2>
-          </div>
-          <div className='scorecard'>
-            <h2 >Player <span id="playerScore">0 </span></h2>
-            <h2 >Computer  <span id="computerScore">0</span></h2>
-            <h2 >Tie  <span id="tieScore">0</span></h2>
-          </div>
+      <div className='result'>
+        <h2 className='resultbox'>Winner of the match is <span id="resultinfo">{winner}</span></h2>
+        <div>
+          <button onClick={Newgame}>Start New Game</button>
         </div>
-
-        <div className='result'>
-             <h2 className='resultbox'>Winner of the match is <span id="resultinfo">Loading...</span></h2>
-          <div >
-              <button onClick={Newgame}>Start New Game</button>
-          </div>
-        </div>
+      </div>
     </div>
-    
   );
 }
 
